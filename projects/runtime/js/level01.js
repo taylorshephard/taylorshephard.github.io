7deb72ee8339400ab9f75ec5fc3bb05e93e15dcc
Taylor <taylorshephard1@gmail.com>
2021-02-23 15:14:13 -0600

    changed layout colors

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -27,7 +27,7 @@ var level01 = function (window) {
         };
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
-        game.setDebugMode(true);
+        game.setDebugMode(false);
 
         // BEGIN EDITING YOUR CODE HERE
         
